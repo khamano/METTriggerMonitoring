1562992fcdc03b0cd5609ba24f34519a588b47fd
rjwang <r.jiewang@gmail.com>
2016-05-18 14:30:37 +0200

    cleanup

--- TrigMonitoring/TrigCostMonitor/x86_64-slc6-gcc48-opt/dict/TrigCostMonitor/TrigCostMonitorDict_gen.h
+++
@@ -1,7 +0,0 @@
-#ifndef TRIGCOSTMONITOR_TRIGCOSTMONITOREVENT_DICT_H
-#define TRIGCOSTMONITOR_TRIGCOSTMONITOREVENT_DICT_H
-
-#include "TrigCostMonitor/ITrigNtTool.h"
-#include "TrigCostMonitor/UtilCost.h"
-
-#endif
